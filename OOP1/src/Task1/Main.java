dff617a6088ef23303e2a18b8a9535b554ff4b1e
Stevespecious <g4m3rkc@gmail.com>
2022-10-06 23:52:12 +0200

    .

--- OOP1/src/Task1/Main.java
+++ OOP1/src/Task1/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Car car = new Car("Mercedes", "G63", 2022, "SUV");
         car.setDriver(driver);
         System.out.println(car + car.getDriver().toString());
-
         Car car1 = new Car("Mercedes", "A220D", 2022, "Hatchback");
         car1.setDriver(driver);
         System.out.println(car1 + car1.getDriver().toString());
