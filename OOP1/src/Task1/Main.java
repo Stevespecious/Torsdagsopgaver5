2222c534696dccb3714bbac7abe0066541bc0348
Stevespecious <g4m3rkc@gmail.com>
2022-10-06 15:19:32 +0200

    .

---
+++ OOP1/src/Task1/Main.java
@@ -0,0 +1,14 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Driver driver = new Driver("Christian", 21);
+        Car car = new Car("Mercedes", "G63", 2022, "SUV");
+        car.setDriver(driver);
+        System.out.println(car + car.getDriver().toString());
+
+        Car car1 = new Car("Mercedes", "A220D", 2022, "Hatchback");
+        car1.setDriver(driver);
+        System.out.println(car1 + car1.getDriver().toString());
+    }
+}
